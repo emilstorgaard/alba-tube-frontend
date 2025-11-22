664c0a2c67f6ec3b89b0cf7bab49607e8588ea49
Emil Storgaard <est@novicell.dk>
2025-11-22 11:53:11 +0100

    Center load spinner

--- src/lib/components/UploadVideoModal.svelte
+++ src/lib/components/UploadVideoModal.svelte
@@ -114,20 +114,36 @@
 			/>
 		</div>
 		
-		<button
-			type="submit"
-			title="Upload"
-			class="w-full bg-green text-white hover:bg-light-green px-4 py-2 rounded-md font-semibold transition duration-300 ease-in-out"
-		>
-			 {#if loading}
-                    <svg class="animate-spin h-5 w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
-                    </svg>
-                {:else}
-                    Upload
-                {/if}
-		</button>
+<button
+    type="submit"
+    title="Upload"
+    class="w-full bg-green text-white hover:bg-light-green px-4 py-2 rounded-md font-semibold transition duration-300 ease-in-out flex items-center justify-center"
+>
+    {#if loading}
+        <svg
+            class="animate-spin h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+            ></circle>
+            <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            ></path>
+        </svg>
+    {:else}
+        Upload
+    {/if}
+</button>
 	</div>
 </form>
 
